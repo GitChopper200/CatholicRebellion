284d5195df2c539fcd96d2eaa3f48f97296348b7
IronFilament <arhvass@gmail.com>
2023-12-04 14:44:01 +0100

    Radar Progress, testing sprite rendering and fadeout. (NOT FINISHED)

--- Assets/Scripts/RadarScripts/RadarRaycast.cs
+++ Assets/Scripts/RadarScripts/RadarRaycast.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class RadarRaycast : MonoBehaviour
 {  
     public float maxRayDistance = 10f;
+    public GameObject radarPingSprite;
+    public float fadeDelay = 2f;
+    public float fadeDuration = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +22,47 @@ public class RadarRaycast : MonoBehaviour
         
         RaycastHit hit;
 
+
         if (Physics.Raycast(ray, out hit, maxRayDistance))
         {
+            if(radarPingSprite != null)
+            {
+                Instantiate(radarPingSprite, hit.point, Quaternion.identity);
+
+                Invoke("FadeOut", fadeDelay);
+
+                Debug.Log("btichass");
+            }
+
             Debug.Log("RadarHit"+hit.transform.name);
         }
         Debug.DrawLine(transform.position,transform.position+transform.forward*maxRayDistance);
     }
+    void FadeOut()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+
+        if(renderer != null)
+        {
+            StartCoroutine(FadeOutCoroutine(renderer));
+        }
+    }
+    IEnumerator FadeOutCoroutine(Renderer renderer)
+    {
+        float alpha = 1f;
+
+        float fadeStep = 1f / fadeDuration;
+
+        while (alpha > 0f)
+        {
+            alpha -= fadeStep*Time.deltaTime;
+            Color newColor=renderer.material.color;
+            newColor.a = alpha;
+            renderer.material.color = newColor;
+
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
 }
+
