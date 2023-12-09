0e5e60bbc9dbb04d3a0baeeb55019e6b080b911d
GitChopper200 <vitus@nuf.dk>
2023-12-09 09:38:06 +0100

    finish

--- Assets/Scripts/Coliders/ColiderLawSquare.cs
+++ Assets/Scripts/Coliders/ColiderLawSquare.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SocialPlatforms.GameCenter;
+
 using UnityEngine.UIElements;
 
 public class ColiderLawSquare : MonoBehaviour
