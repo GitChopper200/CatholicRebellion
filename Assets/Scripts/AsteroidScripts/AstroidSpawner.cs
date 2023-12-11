7d35ea8d5d8e12d20d9c4846f427053a39f55727
IronFilament <arhvass@gmail.com>
2023-12-11 10:45:39 +0100

    AstroidSpawnerChanges

--- Assets/Scripts/AsteroidScripts/AstroidSpawner.cs
+++ Assets/Scripts/AsteroidScripts/AstroidSpawner.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AstroidSpawner : MonoBehaviour
@@ -16,62 +17,89 @@ public class AstroidSpawner : MonoBehaviour
     public float timer;
     public float countDownTimer = 5f;
 
-    
+    private Vector3 initialParentPosition;
+
+    private Vector3 moveDirection;
+
 
     // Start is called before the first frame update
     void Start()
     {
-       
 
-        
+
+
     }
     void Update()
     {
-        timer -= Time.deltaTime;
-
-        if(timer <= 0 )
         {
-            Debug.Log("Timer Countdown Over");
-            StartCoroutine(SpawnCoroutine());
-            ResetTimer();
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                Debug.Log("Timer Countdown Over");
+                StartCoroutine(SpawnCoroutine());
+                ResetTimer();
+            }
+
+        }
+
+        void ResetTimer()
+        {
+            timer = countDownTimer;
+        }
+
+
+        IEnumerator SpawnCoroutine()
+        {
+            initialParentPosition = transform.position;
+
+            moveDirection = (initialParentPosition - transform.position).normalized;
+
+            yield return new WaitForSeconds(spawnDelay);
+
+            SpawnAsteroid();
+        }
+
+        void SpawnAsteroid()
+        {
+            float randomAngle = Random.Range(0f, 360f);
+            float angleInRadians = Mathf.Deg2Rad * randomAngle;
+
+
+            Vector3 spawnPosition = new Vector3(transform.position.x + spawnRadius * Mathf.Cos(angleInRadians),
+                                                transform.position.y + spawnRadius,
+                                                transform.position.z + spawnRadius * Mathf.Cos(angleInRadians)
+            );
+
+
+            Debug.Log("njsdgghjdfs");
+            GameObject spawnedObject = Instantiate(spawnPrefab, spawnPosition, Quaternion.identity);
+
+            // Vector3 initialPosition = spawnedObject.transform.position;
+
+            // Vector3 directionToPlayer = (transform.position-spawnedObject.transform.position).normalized;
+
+            StartCoroutine(MovePrefab(spawnedObject, initialParentPosition));
+        }
+
+        IEnumerator MovePrefab(GameObject obj, Vector3 targetPosition)
+        {
+            float elapsedTime = 0f;
+            Vector3 initialPosition = obj.transform.position;
+
+            while (elapsedTime < speed)
+            {
+                obj.transform.position = Vector3.Lerp(obj.transform.position, targetPosition, elapsedTime / speed);
+
+                elapsedTime += Time.deltaTime;
+
+                yield return null;
+
+            }
+
+            obj.transform.position = transform.position;
+
+
         }
     }
-
-    void ResetTimer()
-    {
-        timer = countDownTimer;
-    }
-
-
-    IEnumerator SpawnCoroutine()
-    {
-       yield return new WaitForSeconds(spawnDelay);
-
-        SpawnAsteroid();
-    }
-
-    void SpawnAsteroid()
-    {
-        float randomAngle = Random.Range(0f, 360f);
-        float angleInRadians = Mathf.Deg2Rad * randomAngle;
-
-        Vector3 spawnPosition = new Vector3(transform.position.x + spawnRadius * Mathf.Cos(angleInRadians),
-                                            transform.position.y + spawnRadius, 
-                                            transform.position.z + spawnRadius * Mathf.Cos(angleInRadians)
-        );
-                                            
-
-        Debug.Log("njsdgghjdfs");
-        GameObject spawnedObject = Instantiate(spawnPrefab, spawnPosition, Quaternion.identity);
-        
-        Vector3 directionToPlayer = (transform.position-spawnedObject.transform.position).normalized;
-
-        while (true)
-        {
-            spawnedObject.transform.position = spawnPosition * speed * Time.deltaTime;
-        }
-        
-    }
-    
-    
 }
